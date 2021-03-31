2401538abf35e264ca1fe8058c86156bce6ed9f4
Joanna <jlazartelagos@gmail.com>
2021-03-31 16:17:47 -0300

    agrego-opciones

--- imagen/imagen.js
+++ imagen/imagen.js
@@ -6,12 +6,46 @@ const elemento = document.querySelector(".img");
 elemento.classList.add(imagen);
 /*
 //SIN CSS + If:
+const imgSize = prompt("Ingrese el tamaño de imagen que desee: "chica", "mediana" o "grande".);
+const elemento = document.querySelector(".img");
+const imagenChica = `width:250px;`;
+const imagenMediana = `width:450px;`;
+const imagenGrande = `width:750px;`;
+if (imgSize == "chica") {
+    elemento.style = imagenChica;
+} else if (imgSize == "mediana") {
+    elemento.style = imagenMediana;
+} else if (imgSize == "grande") {
+    elemento.style = imagenGrande;
+} else {
+    alert("Opción inválida");
+}
 
+*/
 
- */
 /*
+//CON CSS y con If:
+const imgSize = prompt("Ingrese un número del 1 al 3, según el tamaño de imagen que desee: 1) chico, 2) mediano, 3) grande.");
+const elemento = document.querySelector(".img");
+let imagen = "";
+if (imgSize == 1) {
+    imagen = "chica";
+} else if (imgSize == 2) {
+    imagen = "mediana";
+} else if (imgSize == 3) {
+    imagen = "grande";
+} else {
+    alert("Opción inválida");
+}
+
+//elemento.classList.remove("img");
+elemento.classList.add(imagen);
+*/
+
+/*
+//Esto me lo enseñó mi hermano, pero no necesitan entenderlo todavía
 //SIN CSS (eliminando el link al css en en html)
-//y con objeto/mapa + If:
+//y CON OBJETO/MAPA + If:
 
 const imagen = prompt("Ingrese un tamaño de imagen: chica, mediana, grande");
 const elemento = document.querySelector(".img");
@@ -33,21 +67,3 @@ if (imagen in estilos) {
 */
 
 
-/*
-//CON CSS y con If:
-const imgSize = prompt("Ingrese un número del 1 al 3, según el tamaño de imagen que desee: 1) chico, 2) mediano, 3) grande.");
-const elemento = document.querySelector(".img");
-let imagen = "";
-if (imgSize == 1) {
-    imagen = "chica";
-} else if (imgSize == 2) {
-    imagen = "mediana";
-} else if (imgSize == 3) {
-    imagen = "grande";
-} else {
-    alert("Opción inválida");
-}
-
-//elemento.classList.remove("img");
-elemento.classList.add(imagen);
-*/
--- rgb/index.js
+++ rgb/index.js
@@ -13,3 +13,23 @@ const colorTres = prompt("Ingrese un último numero del 0 al 255");
 const cuerpo = document.querySelector("body");
 
 cuerpo.style = `background-color: rgb(${colorUno}, ${colorDos}, ${colorTres});`
+
+//### color
+
+//Hacer un programa que al iniciarse pida mediante un `prompt` ingresar un color en sistema hexadecimal, y una vez ingresado ponga el color de fondo del body con dicho color.
+
+/**
+ 1. pedir al usuario color 
+    a. usar prompt
+ 2. cambiar el color
+    a. obtener el body del html 
+    b. cambiar el color de background 
+ */
+
+// rgb(255,255,255)
+// #FFFFFF
+const color = prompt('ingrese el color');
+
+const body = document.querySelector("body");
+body.style = `background-color: #${color};`;
+
