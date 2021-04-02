a98014c2d6ba1aef486fe80568669c0b3d6acb98
Joanna <jlazartelagos@gmail.com>
2021-04-02 01:06:13 -0300

    completo

--- animales/index.js
+++ animales/index.js
@@ -7,3 +7,20 @@ opciones disponibles) y muestre solamente la
 imagen del animal elegido.
 
  */
+const animalElegido = prompt("¿Perro, gato o hámster?");
+
+const nombreAnimal = document.querySelector("#titulo");
+const imagenAnimal = document.querySelector("#miAnimalFavorito");
+imagenAnimal.style = `width:550px;height:350px;`;
+if (animalElegido == "perro") {
+    nombreAnimal.innerHTML = "Perro";
+    imagenAnimal.src = "./imagenes/perro.jpg";
+} else if (animalElegido == "gato") {
+    nombreAnimal.innerHTML = "Gato";
+    imagenAnimal.src = "./imagenes/gato.jpg";
+} else if (animalElegido == "hámster") {
+    nombreAnimal.innerHTML = "Hámster";
+    imagenAnimal.src = "./imagenes/hamster.jpg";
+} else {
+    alert("Opción inválida");
+};
